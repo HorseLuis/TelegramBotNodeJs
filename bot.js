9f3cda3e5506031f8668ca787b7940b3cb6c22dc
Eric <galletobalu@gmail.com>
2021-02-26 21:51:31 +0100

    Family friendly

--- bot.js
+++ bot.js
@@ -14,24 +14,24 @@ bot.catch((err) => {
 })
 
 bot.start((ctx) => {
-    ctx.reply('Bienvenido. No me toques mucho los cojones, por favor.');
+    ctx.reply('Bienvenido. Espero poder ser de ayuda.');
 })
 
 bot.command('help', (ctx) => {
-    ctx.reply("Soy como una navaja suiza. Valgo para muchas cosas, y si me calientas puedo incluso apuñalarte. ¿No me crees? Echa un vistazo a mis increibles funcionalidades:\r\n\r\n" +
-    "/help - Puedes pedirme ayuda si quieres, puto noname de mierda.\r\n" +
+    ctx.reply("Soy como una navaja suiza, sirvo para muchas cosas. ¿No me crees? Echa un vistazo a mis increibles funcionalidades:\r\n\r\n" +
+    "/help - Puedes pedirme ayuda si quieres.\r\n" +
     "/random valor1,valor2... - Devuelve un valor aleatorio de la lista proporcionada.\r\n" +
-    "/meme - Te mando un pedazo memaso de me_irl.\r\n" +
+    "/meme - Te mando un meme de me_irl.\r\n" +
     "/weahter ubicación - El tiempo en la ubicación seleccionada.\r\n" +
-    "\r\nEsto es todo. Si no te llega, puedes ir a follarte a tu prima.");
+    "\r\nEsto es todo. Tal vez tenga nuevas funciones en el futuro. Stay tuned.");
 })
 
 bot.command('random', (ctx) => {
     var texto = ctx.message.text;
     texto = texto.replace('/random', '').trim();
      if(texto === '') {
-         ctx.reply('A ver, pedazo de escoria. No puedo devolver un resultado aleatorio de una lista QUE NO EXISTE. O sí, puedo devolverte a tu novi@.');
-         ctx.replyWithSticker('https://tlgrm.eu/_/stickers/8a1/9aa/8a19aab4-98c0-37cb-a3d4-491cb94d7e12/4.webp');
+         ctx.reply('Debes proporcionarme una lista de valores.');
+        //  ctx.replyWithSticker('https://tlgrm.eu/_/stickers/8a1/9aa/8a19aab4-98c0-37cb-a3d4-491cb94d7e12/4.webp');
      } else {
         var opciones = texto.split(',');
         var rand = Math.floor(Math.random() * (opciones.length));
@@ -46,7 +46,7 @@ bot.command('meme', (ctx) => {
         .then(res => {
             const data = res.data.data;
             if (data.children.length < 1) {
-                ctx.reply('No hay meme. Te jodes.');
+                ctx.reply('Hoy no hay memes.');
             }
 
             var rand = Math.floor(Math.random() * 100);
@@ -56,10 +56,10 @@ bot.command('meme', (ctx) => {
                 if (img.endsWith('jpg') || img.endsWith('png')){
                     ctx.replyWithPhoto({url: data.children[rand].data.url});
                 } else {
-                    ctx.reply('No hay meme. Te jodes.');
+                    ctx.reply('Hoy no hay memes.');
                 }
             } catch (error) {
-                ctx.reply('No hay meme. Te jodes.');
+                ctx.reply('Hoy no hay memes.');
             }
         })
 })
@@ -68,7 +68,7 @@ bot.command('weather', (ctx) => {
     var texto = ctx.message.text;
     texto = texto.replace('/weather', '').trim();
     if(texto === '') {
-        ctx.reply('Como no me des una ubicación, voy a buscarte y te pego un tiro. IMBECIL');
+        ctx.reply('Necesito una ubicación para poder proporcionarte datos del clima.');
     } else {
         let city = texto;
         let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weather_key}&units=metric&lang=es`
@@ -91,18 +91,18 @@ const regex3 = new RegExp(/.*BOT.*/i);
 
 bot.hears([regex1,regex2,regex3], ctx => {
     if (!ctx.message.text.includes('@horse_luis_bot')){
-        ctx.reply('He visto que me has mencionado. ¿Tienes algún problema, pedazo de subnormal?');
+        ctx.reply('He visto que me has mencionado. ¿Tienes algún problema?');
     }
 })
 
 bot.on('new_chat_participant', ctx => {
     const user = ctx.update.message.new_chat_participant.first_name;
-    ctx.reply(`Bienvenid@, ${user}. No toques mucho los huevos o te mando a la mierda.`);
+    ctx.reply(`Bienvenid@, ${user}. Disfruta de la estancia.`);
 })
 
 bot.on('left_chat_participant', ctx => {
     const user = ctx.update.message.left_chat_participant.first_name;
-    ctx.reply(`Venga, ${user}, a tomar por culo.`);
+    ctx.reply(`Nos vemos, ${user}.`);
 })
 
 bot.launch();
